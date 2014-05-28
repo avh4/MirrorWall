e5caf0e68d9bc0e0422611cb20b3d5bcdc1b0592
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-27 22:27:56 -0700

    Dropbox datastore spike

--- src/main/js/MirrorWall.js
+++ src/main/js/MirrorWall.js
@@ -5,7 +5,12 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  doUpdate: function() {
+    this.forceUpdate();
+  },
   render: function() {
-    return <div><h1>MirrorWall</h1></div>;
+    var projects = this.props.datastore.getTable('projects');
+    var p = projects.query();
+    return <div><h1>MirrorWall {p.length}</h1></div>;
   }
 });
--- src/main/js/main.js
+++ src/main/js/main.js
@@ -5,4 +5,41 @@
 var React = require('react');
 var MirrorWall = require('./MirrorWall');
 
-React.renderComponent(<MirrorWall/>, document.getElementById('root'));
+var client = new Dropbox.Client({key: 'hzy00wu1zd7neiv'});
+
+// Try to finish OAuth authorization.
+client.authenticate({interactive: false}, function (error) {
+    if (error) {
+        alert('Authentication error: ' + error);
+    }
+});
+
+if (client.isAuthenticated()) {
+  console.log("AUTHED!");
+    // Client is authenticated. Display UI.
+    
+    var datastoreManager = client.getDatastoreManager();
+    datastoreManager.openDefaultDatastore(function (error, datastore) {
+        if (error) {
+            alert('Error opening default datastore: ' + error);
+        }
+
+        // Now you have a datastore. The next few examples can be included here.
+        
+        var projects = datastore.getTable('projects');
+        projects.insert({test: new Date().toString()});
+        
+        var mw = <MirrorWall datastore={datastore}/>;
+        React.renderComponent(mw, document.getElementById('root'));
+
+        datastore.recordsChanged.addListener(function (event) {
+            console.log('records changed:', event.affectedRecordsForTable('projects'));
+            mw.doUpdate();
+        });
+    });
+    
+} else {
+  console.log("NOT AUTHED");
+  client.reset();
+  client.authenticate();
+}
