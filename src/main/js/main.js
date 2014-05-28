0298f2f5686af491e4242285496e7f109beeff3e
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-27 21:53:24 -0700

    Initialize project.

---
+++ src/main/js/main.js
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require('react');
+var MirrorWall = require('./MirrorWall');
+
+React.renderComponent(<MirrorWall/>, document.getElementById('root'));
