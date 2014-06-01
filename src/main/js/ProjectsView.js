fe6b4247fadf18deb72d8ac95665bbd1377bba18
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-01 12:08:28 -0700

    Fix react keys in ProjectsView

--- src/main/js/ProjectsView.js
+++ src/main/js/ProjectsView.js
@@ -10,7 +10,7 @@ module.exports = React.createClass({
       return <div>No project data provided</div>;
     }
     var cards = this.props.projects.map(function(project, i) {
-      return <div className="col-xs-4 col-sm-3 col-lg-2"><div className="project-card" style={{"background-color": "#f77"}} key={i}>{project}</div></div>;
+      return <div className="col-xs-4 col-sm-3 col-lg-2" key={i}><div className="project-card" style={{"background-color": "#f77"}}>{project}</div></div>;
     }, this);
     return <div className="row">{cards}</div>;
   }
