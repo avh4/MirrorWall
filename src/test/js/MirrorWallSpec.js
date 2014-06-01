592f15cdee8f45e16966e9714ef84bcbdfb0659c
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-01 12:07:39 -0700

    Mock ProjectsView in MirrorWallSpec

--- src/test/js/MirrorWallSpec.js
+++ src/test/js/MirrorWallSpec.js
@@ -1,12 +1,30 @@
 /** @jsx React.DOM */
 
+var rewire = require('rewire');
 var ReactTest = require('./ReactTest');
 var expect = require('chai').expect;
-var MirrorWall = require('../../main/js/MirrorWall');
+var MirrorWall = rewire('../../main/js/MirrorWall');
+
+var React = require('react');
+
+function stubComponent(subject, componentNameToMock) {
+  var mock = React.createClass({
+    render: function() {
+      return <div id={componentNameToMock}>{JSON.stringify(this.props)}</div>;
+    }
+  });
+  MirrorWall.__set__(componentNameToMock, mock);
+}
+
+stubComponent(MirrorWall, 'ProjectsView');
+
+function component(element) {
+  return JSON.parse(element.text());
+}
 
 describe('MirrorWall', function() {
   it('shows projects', function() {
     var $ = ReactTest.render(<MirrorWall projects={['Project wall']}/>);
-    expect($('.project-card').text()).to.eql('Project wall');
+    expect(component($('#ProjectsView')).projects).to.eql(['Project wall']);
   });
 });
