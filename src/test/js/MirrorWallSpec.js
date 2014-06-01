5f3b86f634aab0d02064fa7540b93e47b8e9b29b
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-01 12:19:09 -0700

    Extract ReactTest.mock.props from MirrorWallSpec

--- src/test/js/MirrorWallSpec.js
+++ src/test/js/MirrorWallSpec.js
@@ -2,18 +2,15 @@
 
 var rewire = require('rewire');
 var ReactTest = require('./ReactTest');
+var props = ReactTest.mock.props;
 var expect = require('chai').expect;
 var MirrorWall = rewire('../../main/js/MirrorWall');
 
 ReactTest.mock(MirrorWall, 'ProjectsView');
 
-function component(element) {
-  return JSON.parse(element.text());
-}
-
 describe('MirrorWall', function() {
   it('shows projects', function() {
     var $ = ReactTest.render(<MirrorWall projects={['Project wall']}/>);
-    expect(component($('#ProjectsView')).projects).to.eql(['Project wall']);
+    expect(props($('#ProjectsView')).projects).to.eql(['Project wall']);
   });
 });
