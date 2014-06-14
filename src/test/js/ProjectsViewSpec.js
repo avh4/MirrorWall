3e04300c95485fc791c5c1c58fa4c9d4814f8d7f
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-13 22:14:34 -0700

    Restore ProjectsViewSpec

--- src/test/js/ProjectsViewSpec.js
+++ src/test/js/ProjectsViewSpec.js
@@ -1,37 +1,38 @@
-// /** @jsx React.DOM */
-// 
-// require('./env');
-// var ReactTest = require('./ReactDomTest');
-// var props = ReactTest.mock.props;
-// var ProjectsView = rewire('../../main/js/ProjectsView');
-// 
-// ReactTest.mock(ProjectsView, 'ProjectCard', function() { return { name: this.project.get('name') } });
-// 
-// function p(name) {
-//   return { get: sinon.stub().withArgs('name').returns(name) };
-// }
-// 
-// function getName(card) {
-//   return props(card).name;
-// }
-// 
-// describe('ProjectsView', function() {
-//   beforeEach(ReactTest.reset);
-// 
-//   it('shows projects', function() {
-//     ReactTest.render(<ProjectsView projects={[p('Project wall')]}/>);
-//     expect(props('#ProjectCard1').name).to.equal('Project wall');
-//   });
-// 
-//   it('shows all projects', function() {
-//     ReactTest.render(<ProjectsView projects={[p('Project wall'), p('Econ 101')]}/>);
-//     expect($('.ProjectCard').get().map(getName)).to.eql(['Project wall', 'Econ 101']);
-//   });
-// 
-//   describe('with no data', function() {
-//     it('shows a message', function() {
-//       ReactTest.render(<ProjectsView/>);
-//       expect($('div').text()).to.eql('No project data provided');
-//     });
-//   });
-// });
+require('./env');
+
+var ProjectCard = {
+  render: function(project) {
+    return '%Project:' + project.get('name');
+  }
+};
+
+var ProjectsView = subject('../../main/js/ProjectsView', {
+  ProjectCard: ProjectCard
+});
+
+function p(name) {
+  return { get: sinon.stub().withArgs('name').returns(name) };
+}
+
+describe('ProjectsView', function() {
+  it('shows projects', function() {
+    var p1 = p('Project wall');
+    var $ = new MercuryTest(ProjectsView.render, mercury.value([p1]));
+    expect($('div.row div').text()).to.equal('%Project:Project wall');
+  });
+
+  it('shows all projects', function() {
+    var p1 = p('Project wall');
+    var p2 = p('Econ 101');
+    var $ = new MercuryTest(ProjectsView.render, mercury.value([p1, p2]));
+    expect($('div.row div').first().text()).to.equal('%Project:Project wall');
+    expect($('div.row div').last().text()).to.equal('%Project:Econ 101');
+  });
+
+  describe('with no data', function() {
+    it('shows a message', function() {
+      var $ = new MercuryTest(ProjectsView.render, mercury.value());
+      expect($('div').first().text()).to.eql('No project data provided');
+    });
+  });
+});
