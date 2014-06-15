624d9e8792833907d01758e23bcf92d428cc924c
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-14 20:41:07 -0700

    Extract $ in MercuryTest.js

--- src/test/js/MercuryTest.js
+++ src/test/js/MercuryTest.js
@@ -5,6 +5,24 @@ var event = require('synthetic-dom-events');
 var Document = require('min-document/document');
 var cheerio = require('cheerio');
 
+function $(document, elem, selector) {
+  var result = cheerio.load(elem.toString())(selector);
+  var className = undefined;
+  if (selector[0] === '.') {
+    className = selector.substring(1);
+  }
+  result.input = function(text) {
+    if (!className) throw new Error('only currently implemented for class selectors');
+    document.getElementsByClassName(className)[0].value = text;
+    document.getElementsByClassName(className)[0].dispatchEvent(event('input'));
+  };
+  result.click = function() {
+    if (!className) throw new Error('only currently implemented for class selectors');
+    document.getElementsByClassName(className)[0].dispatchEvent(event('click'));
+  };
+  return result;
+};
+
 function MercuryTest(/*...*/) {
   var state, render;
   if (arguments.length == 1) {
@@ -19,24 +37,7 @@ function MercuryTest(/*...*/) {
   var div = document.createElement('div');
   document.body.appendChild(div);
   mercury.app(div, state, render, {document: document});
-  var $ = function(selector) {
-    var result = cheerio.load(div.toString())(selector);
-    var className = undefined;
-    if (selector[0] === '.') {
-      className = selector.substring(1);
-    }
-    result.input = function(text) {
-      if (!className) throw new Error('only currently implemented for class selectors');
-      document.getElementsByClassName(className)[0].value = text;
-      document.getElementsByClassName(className)[0].dispatchEvent(event('input'));
-    };
-    result.click = function() {
-      if (!className) throw new Error('only currently implemented for class selectors');
-      document.getElementsByClassName(className)[0].dispatchEvent(event('click'));
-    };
-    return result;
-  };
-  return $;
+  return $.bind(null, document, div);
 }
 
 module.exports = MercuryTest;
